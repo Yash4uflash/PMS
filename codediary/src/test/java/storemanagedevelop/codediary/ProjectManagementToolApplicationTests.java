96efd6f027ff78bb0832c798730a09810b2741cf
Yash4uflash <yashwanth.pdn@gmail.com>
2022-10-16 20:28:06 +0530

    first commit

---
+++ codediary/src/test/java/storemanagedevelop/codediary/ProjectManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package storemanagedevelop.codediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
